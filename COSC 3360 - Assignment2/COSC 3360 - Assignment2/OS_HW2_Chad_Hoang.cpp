d3269c08c38ac7e1478d64d3f2541e4b36f2efa9
Chad Hoang <chad.hoang@yahoo.com>
2016-11-01 13:02:32 -0500

    Set up C++ VS project

---
+++ COSC 3360 - Assignment2/COSC 3360 - Assignment2/OS_HW2_Chad_Hoang.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <sstream>
+
+using namespace std;
+
+/*
+Assignment 2
+COSC 3360 - Fundamentals of Operating Systems
+University of Houston
+Chad Hoang
+1176413
+*/
+
+// Variables
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
