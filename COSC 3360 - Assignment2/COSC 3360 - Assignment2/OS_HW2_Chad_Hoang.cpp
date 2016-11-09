3513a032f9bab7484256b913dd2d83cb7a08b9ad
Chad Hoang <chad.hoang@yahoo.com>
2016-11-09 13:41:11 -0600

    Parsed the input and processes variables into their respective arrays

--- COSC 3360 - Assignment2/COSC 3360 - Assignment2/OS_HW2_Chad_Hoang.cpp
+++ COSC 3360 - Assignment2/COSC 3360 - Assignment2/OS_HW2_Chad_Hoang.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <vector>
 #include <sstream>
 
 using namespace std;
@@ -19,7 +20,12 @@ Chad Hoang
 */
 
 // Variables
-int *inputVariables;				//	Array of input variables given by the data file
+vector<string> variables;				//	Array of input variables given by the data file
+vector<string> processes;					//	Array of input variables given by the data file
+
+//	Methods
+void ReadFromFile(string codeFileName, string dataFileName);
+vector<string> ParseVariablesInString(string startingString, string inputString);
 
 int main(int argc, char* argv[])
 {
@@ -38,6 +44,63 @@ void ReadFromFile(string codeFileName, string dataFileName)
 	//	Evaluate the input file
 	if (codeFile.is_open())
 	{
+		//	Get the first line
+		string currentLine;
+		
+		//	Loop through lines until "input_var" is found
+		while (getline(codeFile, currentLine))
+		{			
+			if (currentLine.find("input_var") != std::string::npos)
+			{
+				//	Cache the parse variables in the string into our variables string array
+				variables = ParseVariablesInString(" ", currentLine);
+				break;
+			}
+		}
+		
+		//	Fetch next line
+		getline(codeFile, currentLine);
 
+		//	Cache the parse variables in the string into our processes string array
+		processes = ParseVariablesInString(" ", currentLine);
 	}
 }
+
+#pragma region ParseVariablesInString: parses variables in the given string starting from the delimiter
+vector<string> ParseVariablesInString(string startingString, string inputString)
+{
+	vector<string> parsedArray;
+
+	//	Remove "input_var" and ";" from string so its easier to parse & remove
+	size_t foundIndex = inputString.find(startingString);
+	inputString.erase(0, foundIndex + 1);
+	foundIndex = inputString.find(";");
+	if (foundIndex != -1)
+		inputString.erase(foundIndex, foundIndex);
+
+	//	Parse variables from current line to string array
+	string delimiter = ",";
+	size_t pos = 0;
+	string token;
+	int count = 0;
+	while ((pos = inputString.find(delimiter)) != string::npos)
+	{
+		token = inputString.substr(0, pos);
+
+		//	Add to variables array
+		parsedArray.push_back(token);
+
+		//	Erase previous token so we can parse next delimiter
+		inputString.erase(0, pos + delimiter.length());
+	}
+	//	Add last token to variables array
+	parsedArray.push_back(token);
+
+	// Display
+	for (size_t i = 0; i < parsedArray.size(); i++)
+		cout << parsedArray[i] << " ";
+	cout << endl;
+
+	return parsedArray;
+}
+#pragma endregion
